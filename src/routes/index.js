332a5106b83fb157ad98098aad2be78b0c2feb93
Firmansyah Otoluwa <pirmansah.otoluwa@gmail.com>
2022-01-08 23:05:40 +0700

    Fix route path redirect to /home by default

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
+import { Redirect } from "react-router-dom";
 import Experience from "../screens/Expoerience";
 import Home from "../screens/Home";
 
 const routes = [
     { path: "/home", exact: true, component: Home },
     { path: "/experience/:id", component: Experience },
+    { path: "/", exact: true, component: () => <Redirect to="/home  " /> },
 ];
 
 export { routes };
