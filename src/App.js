8fe0cbb182efa69fc8140882c2431b2027756974
Firmansyah Otoluwa <pirmansah.otoluwa@gmail.com>
2022-01-08 11:26:56 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import { HomeLayout } from './layout/HomeLayout';
+
+function App() {
+  return (
+    <React.Fragment>
+      <HomeLayout />
+    </React.Fragment>
+  );
+}
+
+export default App;
